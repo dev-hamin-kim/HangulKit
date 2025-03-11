34102c6c6fd4a931ac8d955d6950739afcb17221
dev-hamin-kim <129831340+dev-hamin-kim@users.noreply.github.com>
2025-03-11 20:33:00 +0900

    Initial Commit

---
+++ HangulKit/HangulKit.h
@@ -0,0 +1,18 @@
+//
+//  HangulKit.h
+//  HangulKit
+//
+//  Created by 김하민 on 3/11/25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for HangulKit.
+FOUNDATION_EXPORT double HangulKitVersionNumber;
+
+//! Project version string for HangulKit.
+FOUNDATION_EXPORT const unsigned char HangulKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HangulKit/PublicHeader.h>
+
+
---
+++ HangulKitTests/HangulKitTests.swift
@@ -0,0 +1,17 @@
+//
+//  HangulKitTests.swift
+//  HangulKitTests
+//
+//  Created by 김하민 on 3/11/25.
+//
+
+import Testing
+@testable import HangulKit
+
+struct HangulKitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
