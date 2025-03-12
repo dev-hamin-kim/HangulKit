f3a488ebfbace5dc1580e40abbeadafd17681e82
dev-hamin-kim <129831340+dev-hamin-kim@users.noreply.github.com>
2025-03-12 23:17:35 +0900

    test: HangulKitWrapper 테스트 코드 작성

--- HangulKitTests/HangulKitTests.swift
+++ HangulKitTests/HangulKitTests.swift
@@ -10,14 +10,22 @@ import Testing
 
 struct HangulKitTests {
 
+    // isHangulSyllable은 완성된 한글 문자를 받으면 true를 반환한다.
     @Test func isHangulSyllable() async throws {
-        let hangul = Hangul()
-                
-        #expect(hangul.isHangulSyllable("가") == true)
-        #expect(hangul.isHangulSyllable("값") == true)
-        #expect(hangul.isHangulSyllable("ㄱ") == false)
-        #expect(hangul.isHangulSyllable("ㅏ") == false)
-        #expect(hangul.isHangulSyllable("a") == false)
+        #expect("가".hangul.isHangulSyllable() == true)
+        #expect("값".hangul.isHangulSyllable() == true)
+        #expect("ㄱ".hangul.isHangulSyllable() == false)
+        #expect("ㅏ".hangul.isHangulSyllable() == false)
+        #expect("a".hangul.isHangulSyllable() == false)
+    }
+    
+    // isHangulAlphabet은 조합되지않은 한글 문자를 받으면 true를 반환한다.
+    @Test func isHangulAlphabet() async throws {
+        #expect("가".hangul.isHangulAlphabet() == false)
+        #expect("값".hangul.isHangulAlphabet() == false)
+        #expect("ㄱ".hangul.isHangulAlphabet() == true)
+        #expect("ㅏ".hangul.isHangulAlphabet() == true)
+        #expect("a".hangul.isHangulAlphabet() == false)
     }
 
 }
