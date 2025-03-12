22b574f5778f02f51b81b256d971bc6395e3d520
dev-hamin-kim <129831340+dev-hamin-kim@users.noreply.github.com>
2025-03-12 23:16:43 +0900

    feat: String과 NSString에 HangulKitWrapper 적용

---
+++ HangulKit/Sources/Extensions/NSString+HangulKit.swift
@@ -0,0 +1,21 @@
+//
+//  NSString+HangulKit.swift
+//  HangulKit
+//
+//  Created by 김하민 on 3/12/25.
+//
+
+extension HangulKitWrapper where Base: NSString {
+    
+    public func isHangulSyllable() -> Bool {
+        let rangeOfHangul = "가"..."힣"
+        
+        return rangeOfHangul.contains(base as String)
+    }
+    
+    public func isHangulAlphabet() -> Bool {
+        let rangeOfHangulAlphabet = "ㄱ"..."ㅣ"
+        
+        return rangeOfHangulAlphabet.contains(base as String)
+    }
+}
---
+++ HangulKit/Sources/Extensions/String+HangulKit.swift
@@ -0,0 +1,21 @@
+//
+//  String+HangulKit.swift
+//  HangulKit
+//
+//  Created by 김하민 on 3/12/25.
+//
+
+extension HangulKitWrapper where Base == String {
+    
+    public func isHangulSyllable() -> Bool {
+        let rangeOfHangul = "가"..."힣"
+        
+        return rangeOfHangul.contains(base)
+    }
+    
+    public func isHangulAlphabet() -> Bool {
+        let rangeOfHangulAlphabet = "ㄱ"..."ㅣ"
+        
+        return rangeOfHangulAlphabet.contains(base)
+    }
+}
