4351362f22a388e6ec959b6879adf3e9299ca6cb
dev-hamin-kim <129831340+dev-hamin-kim@users.noreply.github.com>
2025-03-25 21:52:15 +0900

    test: Constants 자료형 변경에 따른 테스트 케이스 변경

--- HangulKitTests/canBeTests.swift
+++ HangulKitTests/canBeTests.swift
@@ -11,28 +11,38 @@ import Testing
 struct canBeTests {
     
     @Test func canBeChoseong() async throws {
+        // 초성이 될 수 있다고 판단되는 경우
         #expect(Hangul.canBeChoseong("ㄱ") == true)
         #expect(Hangul.canBeChoseong("ㅃ") == true)
-        #expect(Hangul.canBeChoseong("ㄳ") == false)
+        
+        // 초성이 될 수 없다고 판단되는 경우
         #expect(Hangul.canBeChoseong("ㅏ") == false)
+        #expect(Hangul.canBeChoseong("ㅘ") == false)
+        #expect(Hangul.canBeChoseong("ㄱㅅ") == false)
         #expect(Hangul.canBeChoseong("가") == false)
     }
     
     @Test func canBeJungseong() async throws {
+        // 중성이 될 수 있다고 판단되는 경우
+        #expect(Hangul.canBeJungseong("ㅗㅏ") == true)
         #expect(Hangul.canBeJungseong("ㅏ") == true)
-        #expect(Hangul.canBeJungseong("ㅘ") == true)
-//        #expect(Hangul.canBeJungseong("ㅏㅗ") == false)
+        
+        // 중성이 될 수 없다고 판단되는 경우
         #expect(Hangul.canBeJungseong("ㄱ") == false)
-        #expect(Hangul.canBeJungseong("ㄳ") == false)
+        #expect(Hangul.canBeJungseong("ㄱㅅ") == false)
         #expect(Hangul.canBeJungseong("가") == false)
     }
     
     @Test func canBeJongseong() async throws {
+        // 종성이 될 수 있다고 판단되는 경우
         #expect(Hangul.canBeJongseong("ㄱ") == true)
-        #expect(Hangul.canBeJongseong("ㄳ") == true)
-//        #expect(Hangul.canBeJongseong("ㅎㄹ") == false)
-        #expect(Hangul.canBeJongseong("가") == false)
+        #expect(Hangul.canBeJongseong("ㄱㅅ") == true)
+        #expect(Hangul.canBeJongseong("ㅂㅅ") == true)
+        
+        // 종성이 될 수 없다고 판단되는 경우
+        #expect(Hangul.canBeJongseong("ㅎㄹ") == false)
+        #expect(Hangul.canBeJongseong("ㅗㅏ") == false)
         #expect(Hangul.canBeJongseong("ㅏ") == false)
-        #expect(Hangul.canBeJongseong("ㅘ") == false)
+        #expect(Hangul.canBeJongseong("ㅏ") == false)
     }
 }
