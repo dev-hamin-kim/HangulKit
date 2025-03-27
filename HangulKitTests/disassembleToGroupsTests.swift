610aefff766276261f403e38bf32228153551225
dev-hamin-kim <129831340+dev-hamin-kim@users.noreply.github.com>
2025-03-27 22:59:44 +0900

    test: disassembleToGroups 테스트 코드 작성

---
+++ HangulKitTests/disassembleToGroupsTests.swift
@@ -0,0 +1,30 @@
+//
+//  disassembleToGroupsTests.swift
+//  HangulKit
+//
+//  Created by 김하민 on 3/25/25.
+//
+
+import Testing
+@testable import HangulKit
+
+struct disassembleToGroupsTests {
+    
+    @Test
+    func disassembleToGroups() {
+        #expect(Hangul.disassembleToGroups("값") == [["ㄱ", "ㅏ", "ㅂ", "ㅅ"]])
+        #expect(Hangul.disassembleToGroups("값이 비싸다") ==
+                [
+                    ["ㄱ", "ㅏ", "ㅂ", "ㅅ"],
+                    ["ㅇ", "ㅣ"],
+                    [" "],
+                    ["ㅂ", "ㅣ"],
+                    ["ㅆ", "ㅏ"],
+                    ["ㄷ", "ㅏ"],
+                ]
+        )
+        #expect(Hangul.disassembleToGroups("사과 짱") == [["ㅅ", "ㅏ"], ["ㄱ", "ㅗ", "ㅏ"], [" "], ["ㅉ", "ㅏ", "ㅇ"]])
+        #expect(Hangul.disassembleToGroups("ㄵ") == [["ㄴ", "ㅈ"]])
+        #expect(Hangul.disassembleToGroups("ㅘ") == [["ㅗ", "ㅏ"]])
+    }
+}
