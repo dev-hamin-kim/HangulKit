5f692316de643a362e2a0b797af1e552eb49cf7e
dev-hamin-kim <129831340+dev-hamin-kim@users.noreply.github.com>
2025-03-27 22:59:27 +0900

    feat: disassembleToGroups 메서드 구현

---
+++ HangulKit/Sources/disassembleToGroups.swift
@@ -0,0 +1,53 @@
+//
+//  disassembleToGroups.swift
+//  HangulKit
+//
+//  Created by 김하민 on 3/25/25.
+//
+
+public extension Hangul {
+    
+    static func disassembleToGroups(_ string: String) -> [[Character]] {
+        var result: [[Character]] = []
+        
+        for char in string {
+            guard char != " " else {
+                result.append([" "])
+                continue
+            }
+            
+//            guard Hangul.DisassembledVowels[char] == nil else {
+//                continue
+//            }
+//            
+//            if let char = Hangul.DisassembledConsonants[char] { }
+
+            var disassembledCharacter = try? disassembleCompleteCharacter(char)
+            
+            guard let disassembledCharacter else {
+                if let char = Hangul.DisassembledVowels[char] { result.append(char) }
+                if let char = Hangul.DisassembledConsonants[char] { result.append(char) }
+                continue
+            }
+            
+            let choseong = disassembledCharacter.choseong
+            let jungseong = disassembledCharacter.jungseong
+            let decomposedJungseong = Hangul.DisassembledVowels[jungseong]
+            
+            var decomposedJamo: [Character] = [choseong]
+            decomposedJamo.append(contentsOf: decomposedJungseong!)
+            
+            guard let jongseong = disassembledCharacter.jongseong else {
+                result.append(decomposedJamo)
+                continue
+            }
+            
+            let decomposedJongseong = Hangul.DisassembledConsonants[jongseong]
+            
+            decomposedJamo.append(contentsOf: decomposedJongseong!)
+            result.append(decomposedJamo)
+        }
+        
+        return result
+    }
+}
