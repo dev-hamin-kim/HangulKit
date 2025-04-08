63519edaf43dccd82ae155234f4d1bf90638c7e7
dev-hamin-kim <129831340+dev-hamin-kim@users.noreply.github.com>
2025-04-08 20:07:02 +0900

    test: binaryAssemble 테스트 코드 작성

---
+++ HangulKitTests/binaryAssembleTests.swift
@@ -0,0 +1,40 @@
+//
+//  binaryAssembleTests.swift
+//  HangulKit
+//
+//  Created by 김하민 on 4/8/25.
+//
+
+import Testing
+@testable import HangulKit
+
+struct binaryAssembleTests {
+    
+    @Test("문장과 모음을 조합하여 다음 글자를 생성한다.")
+    func assemble1() {
+        #expect(Hangul.binaryAssemble("저는 고양이를 좋아합닏", "ㅏ") == "저는 고양이를 좋아합니다")
+    }
+    
+    @Test("문장과 자음을 조합하여 홑받침을 생성한다.")
+    func assemble2() {
+        #expect(Hangul.binaryAssemble("저는 고양이를 좋아하", "ㅂ") == "저는 고양이를 좋아합")
+    }
+    
+    @Test("문장과 자음을 조합하여 겹받침을 생성한다.")
+    func assemble3() {
+        #expect(Hangul.binaryAssemble("저는 고양이를 좋아합", "ㅅ") == "저는 고양이를 좋아핪")
+    }
+    
+    @Test("조합이 불가능한 자음이 입력되면 단순 Join 한다.")
+    func assemble4() {
+        #expect(Hangul.binaryAssemble("저는 고양이를 좋아합", "ㄲ") == "저는 고양이를 좋아합ㄲ")
+        #expect(Hangul.binaryAssemble("저는 고양이를 좋아합", "ㅂ") == "저는 고양이를 좋아합ㅂ")
+    }
+    
+    @Test("조합이 불가능한 모음이 입력되면 단순 Join 한다.")
+    func assemble5() {
+        #expect(Hangul.binaryAssemble("저는 고양이를 좋아하", "ㅏ") == "저는 고양이를 좋아하ㅏ")
+        #expect(Hangul.binaryAssemble("저는 고양이를 좋아합니다", "ㅜ") == "저는 고양이를 좋아합니다ㅜ")
+    }
+
+}
