d3302862346639f1ff2e401f47a5f8465dac3e19
dev-hamin-kim <129831340+dev-hamin-kim@users.noreply.github.com>
2025-04-09 18:18:31 +0900

    feat: Character 배열을 리턴하는 disassemble 메서드 추가

--- HangulKit/Sources/disassemble.swift
+++ HangulKit/Sources/disassemble.swift
@@ -10,4 +10,9 @@ public extension Hangul {
     static func disassemble(_ word: String) -> String {
         return disassembleToGroups(word).reduce(into: "") { $0 += $1 }
     }
+    
+    // MARK: disassembleToGroups가 애초에 1차원 배열을 리턴하게 해서 시간 복잡도를 줄여볼 것.
+    static func disassemble(_ word: String) -> [Character] {
+        return disassembleToGroups(word).flatMap { $0 }
+    }
 }
