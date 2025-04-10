c55523712a8e7b801d2fa58a06c6f0f898947176
dev-hamin-kim <129831340+dev-hamin-kim@users.noreply.github.com>
2025-04-10 20:06:48 +0900

    test: days 테스트 코드 작성

---
+++ HangulKitTests/daysTests.swift
@@ -0,0 +1,44 @@
+//
+//  daysTests.swift
+//  HangulKit
+//
+//  Created by 김하민 on 4/10/25.
+//
+
+import Testing
+@testable import HangulKit
+
+struct daysTests {
+    
+    @Test
+    func example() {
+        #expect(Hangul.days(1) == "하루")
+        #expect(Hangul.days(2) == "이틀")
+        #expect(Hangul.days(3) == "사흘")
+        #expect(Hangul.days(4) == "나흘")
+        #expect(Hangul.days(5) == "닷새")
+        #expect(Hangul.days(6) == "엿새")
+        #expect(Hangul.days(7) == "이레")
+        #expect(Hangul.days(8) == "여드레")
+        #expect(Hangul.days(9) == "아흐레")
+        #expect(Hangul.days(10) == "열흘")
+        #expect(Hangul.days(11) == "열하루")
+        #expect(Hangul.days(20) == "스무날")
+        #expect(Hangul.days(21) == "스무하루")
+        #expect(Hangul.days(30) == "서른날")
+    }
+    
+    
+    // MARK: 아래 테스트 코드에 대한 생각을 해볼 것.
+    // 근데... Swift Integer 타입 쓰면 저것들 다 걸러지잖어...
+    // 0이랑 음수들은 함수 가드문에서 걸렀고...
+//
+//      const invalidNumbers = [0, -1, 31, 1.1, -1.1, Infinity, -Infinity, NaN];
+//
+//      invalidNumbers.forEach(num => {
+//        it(`유효하지 않은 숫자 ${num}에 대해 오류를 발생시켜야 한다.`, () => {
+//          expect(() => days(num)).toThrow('지원하지 않는 숫자입니다.');
+//        });
+//      });
+//    });
+}
