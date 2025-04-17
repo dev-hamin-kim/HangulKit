feedfb7f948c0f73fb84cc0e5903ce055b8e4bad
dev-hamin-kim <129831340+dev-hamin-kim@users.noreply.github.com>
2025-04-17 19:01:02 +0900

    test: transform13thAnd14th 메서드 테스트 코드 작성

---
+++ HangulKitTests/standardizePronunciationTests/rulesTests/transform13thAnd14thTests.swift
@@ -0,0 +1,40 @@
+//
+//  transform13thAnd14thTests.swift
+//  HangulKit
+//
+//  Created by 김하민 on 4/17/25.
+//
+
+import Testing
+@testable import HangulKit
+
+struct transform13thAnd14thTests {
+    typealias Syllable = Hangul.Syllable
+    
+    @Test("제13항 - 홑받침이나 쌍받침이 모음으로 시작된 조사나 어미, 접미사와 결합되는 경우에는, 제 음가대로 뒤 음절 첫소리로 옮겨 발음한다")
+    func transform13th() {
+        let current = try! Hangul.disassembleCompleteCharacter("깎")
+        let next = try! Hangul.disassembleCompleteCharacter("아")
+        
+        let transformed: (Syllable, Syllable) = (
+            Syllable(choseong: "ㄲ", jungseong: "ㅏ"),
+            Syllable(choseong: "ㄲ", jungseong: "ㅏ")
+        )
+        
+        #expect(Hangul.transform13And14th(currentSyllable: current, nextSyllable: next) == transformed)
+    }
+    
+    @Test("제14항 - 겹받침이 모음으로 시작된 조사나 어미, 접미사와 결합되는 경우에는, 뒤엣것만을 뒤 음절 첫소리로 옮겨 발음한다.")
+    func transform14th() {
+        let current = try! Hangul.disassembleCompleteCharacter("닭")
+        let next = try! Hangul.disassembleCompleteCharacter("을")
+        
+        let transformed: (Syllable, Syllable) = (
+            Syllable(choseong: "ㄷ", jungseong: "ㅏ", jongseong: "ㄹ"),
+            Syllable(choseong: "ㄱ", jungseong: "ㅡ", jongseong: "ㄹ")
+        )
+        
+        #expect(Hangul.transform13And14th(currentSyllable: current, nextSyllable: next) == transformed)
+    }
+    
+}
