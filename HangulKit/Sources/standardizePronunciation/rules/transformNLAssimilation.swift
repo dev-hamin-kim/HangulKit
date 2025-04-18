f67b197eb7460aeb4eb21a09a8c4c70fb9deafb8
dev-hamin-kim <129831340+dev-hamin-kim@users.noreply.github.com>
2025-04-18 22:29:25 +0900

    feat: transformNLAssimilation 메서드 내부적으로 사용할 함수 구현

---
+++ HangulKit/Sources/standardizePronunciation/rules/transformNLAssimilation.swift
@@ -0,0 +1,44 @@
+//
+//  transformNLAssimilation.swift
+//  HangulKit
+//
+//  Created by 김하민 on 4/18/25.
+//
+
+public extension Hangul {
+    
+    static fileprivate func applyㄴㄹ덧남(current: Syllable, next: Syllable) -> (Syllable, Syllable) {
+        guard let currentJongseong = current.jongseong else { return (current, next) }
+        
+        guard ㄴㄹ이_덧나는_모음.contains(current.jungseong) else {
+            // ㄴ/ㄹ이 되기 위한 조건이지만 현재 음절의 중성의 ∙(아래아)가 하나가 아닐 경우에는 덧나지 않고 연음규칙이 적용된다
+            let newNext = Syllable(choseong: currentJongseong,
+                                   jungseong: next.jungseong,
+                                   jongseong: next.jongseong)
+            
+            return (current, newNext)
+        }
+        
+        var updatedCurrentJongseong: Character?
+        var updatedNextChoseong: Character = " "
+        
+        if ㄴㄹ이_덧나서_받침_ㄴ_변환.contains(currentJongseong) {
+            updatedCurrentJongseong = currentJongseong == "ㄱ" ? "ㅇ" : currentJongseong
+            updatedNextChoseong = "ㄴ"
+        }
+        
+        if ㄴㄹ이_덧나서_받침_ㄹ_변환 == currentJongseong {
+            updatedNextChoseong = "ㄹ"
+        }
+        
+        let updatedCurrent = Syllable(choseong: current.choseong,
+                                      jungseong: current.jungseong,
+                                      jongseong: updatedCurrentJongseong)
+        
+        let updatedNext = Syllable(choseong: updatedNextChoseong,
+                                   jungseong: next.jungseong,
+                                   jongseong: next.jongseong)
+        
+        return (updatedCurrent, updatedNext)
+    }
+}
