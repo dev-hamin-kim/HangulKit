42847f712e90ede8160abcac6a474447f5d586ec
dev-hamin-kim <129831340+dev-hamin-kim@users.noreply.github.com>
2025-04-18 21:49:22 +0900

    feat: transformHardConversion 메서드 작성

---
+++ HangulKit/Sources/standardizePronunciation/rules/transformHardConversion.swift
@@ -0,0 +1,22 @@
+//
+//  transformHardConversion.swift
+//  HangulKit
+//
+//  Created by 김하민 on 4/18/25.
+//
+
+public extension Hangul {
+    static func transformHardConversion(currentSyllable: Syllable, nextSyllable: Syllable) -> Syllable {
+        guard let currentJonseong = currentSyllable.jongseong,
+              let updatedNextChoseong = 된소리[nextSyllable.choseong] else { return nextSyllable }
+        
+        let 제23항조건_충족 = 된소리_받침.contains(currentJonseong)
+        let 제24_25항조건_충족 = 어간_받침.contains(currentJonseong) && nextSyllable.choseong != "ㅂ"
+        
+        guard 제23항조건_충족 || 제24_25항조건_충족 else { return nextSyllable }
+        
+        return Syllable(choseong: updatedNextChoseong,
+                        jungseong: nextSyllable.jungseong,
+                        jongseong: nextSyllable.jongseong)
+    }
+}
