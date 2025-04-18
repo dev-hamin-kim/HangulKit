31b88a6e9ca8fca171e5c9aaed99d7eb2b9fb352
dev-hamin-kim <129831340+dev-hamin-kim@users.noreply.github.com>
2025-04-18 20:24:33 +0900

    feat: transform19th 메서드 작성

---
+++ HangulKit/Sources/standardizePronunciation/rules/transform19th.swift
@@ -0,0 +1,22 @@
+//
+//  transform19th.swift
+//  HangulKit
+//
+//  Created by 김하민 on 4/18/25.
+//
+
+public extension Hangul {
+    
+    static func transform19th(currentSyllable: Syllable, nextSyllable: Syllable) -> Syllable {
+        guard let currentJongseong = currentSyllable.jongseong else { return nextSyllable }
+        
+        let 제19항조건_충족 = 자음동화_받침_ㄴ_변환.contains(currentJongseong) && nextSyllable.choseong == "ㄹ"
+        
+        guard 제19항조건_충족 else { return nextSyllable }
+        
+        return Syllable(choseong: "ㄴ",
+                        jungseong: nextSyllable.jungseong,
+                        jongseong: nextSyllable.jongseong)
+    }
+
+}
