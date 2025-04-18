6954958f6ad26727c240b52c3c8a2314fd3c33d7
dev-hamin-kim <129831340+dev-hamin-kim@users.noreply.github.com>
2025-04-18 20:46:09 +0900

    test: transform20th 메서드 테스트 코드 작성

---
+++ HangulKitTests/standardizePronunciationTests/rulesTests/transform20thTests.swift
@@ -0,0 +1,39 @@
+//
+//  transform20thTests.swift
+//  HangulKit
+//
+//  Created by 김하민 on 4/18/25.
+//
+
+import Testing
+@testable import HangulKit
+
+struct transform20thTests {
+    typealias Syllable = Hangul.Syllable
+    
+    @Test("'ㄴ'은 'ㄹ'의 앞이나 뒤에서 'ㄹ'로 발음한다")
+    func test1() {
+        let current = try! Hangul.disassembleCompleteCharacter("난")
+        let next = try! Hangul.disassembleCompleteCharacter("로")
+        
+        let transformed: (Syllable, Syllable) = (
+            Syllable(choseong: "ㄴ", jungseong: "ㅏ", jongseong: "ㄹ"),
+            Syllable(choseong: "ㄹ", jungseong: "ㅗ")
+        )
+        
+        #expect(Hangul.transform20th(currentSyllable: current, nextSyllable: next) == transformed)
+    }
+    
+    @Test("첫소리 'ㄴ'이 'ㅀ, ㄾ' 뒤에 연결되는 경우에도 'ㄹ'로 발음한다")
+    func test2() {
+        let current = try! Hangul.disassembleCompleteCharacter("닳")
+        let next = try! Hangul.disassembleCompleteCharacter("는")
+        
+        let transformed: (Syllable, Syllable) = (
+            Syllable(choseong: "ㄷ", jungseong: "ㅏ", jongseong: "ㅀ"),
+            Syllable(choseong: "ㄹ", jungseong: "ㅡ", jongseong: "ㄴ")
+        )
+        
+        #expect(Hangul.transform20th(currentSyllable: current, nextSyllable: next) == transformed)
+    }
+}
