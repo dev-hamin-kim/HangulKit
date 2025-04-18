25233cf12df85eca5f705f4767aba3e5f5ab397c
dev-hamin-kim <129831340+dev-hamin-kim@users.noreply.github.com>
2025-04-18 20:12:47 +0900

    feat: transform18th 메서드 작성

---
+++ HangulKit/Sources/standardizePronunciation/rules/transform18th.swift
@@ -0,0 +1,29 @@
+//
+//  transform18th.swift
+//  HangulKit
+//
+//  Created by 김하민 on 4/18/25.
+//
+
+public extension Hangul {
+    
+    static func transform18th(currentSyllable: Syllable, nextSyllable: Syllable) -> Syllable {
+        let 제18항주요조건_충족 = ["ㄴ", "ㅁ"].contains(nextSyllable.choseong)
+        
+        guard let currentJongseong = currentSyllable.jongseong,
+              제18항주요조건_충족 else { return currentSyllable }
+        
+        var newJongseong: Character?
+        
+        if 비음화_받침_ㅇ_변환.contains(currentJongseong) { newJongseong = "ㅇ" }
+        if 비음화_받침_ㄴ_변환.contains(currentJongseong) { newJongseong = "ㄴ" }
+        if 비음화_받침_ㅁ_변환.contains(currentJongseong) { newJongseong = "ㅁ" }
+        
+        let current = Syllable(choseong: currentSyllable.choseong,
+                               jungseong: currentSyllable.jungseong,
+                               jongseong: newJongseong)
+        
+        return current
+    }
+    
+}
