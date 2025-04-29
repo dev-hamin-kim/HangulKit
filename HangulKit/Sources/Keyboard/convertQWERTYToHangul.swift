ccc9919082741add8a69addcb4d1fac2eb98006b
dev-hamin-kim <129831340+dev-hamin-kim@users.noreply.github.com>
2025-04-29 18:43:56 +0900

    refactor: convertQWERTYToHangul 메서드 리팩터링
    1. 기존 구조에서 여러번의 자료형 변환과 고차함수 반복 사용으로 인해 성능상의 손해를 볼 것으로 추정.
    2. 메서드들 간에 String을 전달하는 것이 아닌 Character를 전달함으로써 효율을 향상하였음.

--- HangulKit/Sources/Keyboard/convertQWERTYToHangul.swift
+++ HangulKit/Sources/Keyboard/convertQWERTYToHangul.swift
@@ -7,7 +7,7 @@
 
 public extension Hangul {
     
-    /// QWERTY 자판에서 입력된 영어 알파벳을 동일한 위치의 두벌식 자판에 해당하는 한글 음소로 변환한 후 온전한 한글 문자와 문장으로 변환합니다
+    /// QWERTY 자판에서 입력된 영어 알파벳을 동일한 위치의 두벌식 자판에 해당하는 한글 음소로 변환한 후 온전한 한글 문자와 문장으로 변환합니다.
     ///
     ///     Hangul.convertQWERTYToHangul("abc") // returns "뮻"
     ///     Hangul.convertQWERTYToHangul("ABC") // also returns "뮻"
@@ -16,7 +16,11 @@ public extension Hangul {
     static func convertQWERTYToHangul(_ word: String) -> String {
         if word.isEmpty { return "" }
         
-        return assemble(word.map { convertQWERTYToAlphabet(String($0)) }) // MARK: 이 줄의 비효율성을 고칠 것.
+        var result: [Character] = []
+        
+        word.forEach { result.append(convertQWERTYToAlphabet($0)) }
+        
+        return assemble(result)
     }
 }
 
