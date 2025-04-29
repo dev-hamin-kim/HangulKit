da8babf6353b53bf3185dd2ac08f3852ad37deb2
dev-hamin-kim <129831340+dev-hamin-kim@users.noreply.github.com>
2025-04-29 20:34:23 +0900

    docs: seosusa 메서드 주석 작성

--- HangulKit/Sources/Number/seosusa.swift
+++ HangulKit/Sources/Number/seosusa.swift
@@ -35,6 +35,26 @@ public extension Hangul {
         20: "스무",
     ]
 
+    /// 숫자를 한글 서수사로 변환합니다.
+    ///
+    ///     Hangul.seosusa(of: 1) // returns "첫째"
+    ///     Hangul.seosusa(of: 2) // returns "둘째"
+    ///     Hangul.seosusa(of: 3) // returns "셋째"
+    ///     Hangul.seosusa(of: 10) // returns "열째"
+    ///     Hangul.seosusa(of: 11) // returns "열한째"
+    ///     Hangul.seosusa(of: 12) // returns "열두째"
+    ///     Hangul.seosusa(of: 13) // returns "열셋째"
+    ///     Hangul.seosusa(of: 20) // returns "스무째"
+    ///     Hangul.seosusa(of: 21) // returns "스물한째"
+    ///     Hangul.seosusa(of: 30) // returns "서른째"
+    ///     Hangul.seosusa(of: 40) // returns "마흔째"
+    ///     Hangul.seosusa(of: 99) // returns "아흔아홉째"
+    ///     Hangul.seosusa(of: 100) // returns "백째"
+    ///
+    /// - Note:
+    /// - `서수사`는 순서를 나타내는 단어입니다.
+    /// - 1부터 99까지의 정수는 순우리말 서수사 문자열로 변환합니다.
+    /// - 100 이상의 정수는 한자어 서수사 문자열로 변환합니다.
     static func seosusa(of num: Int) -> String {
         guard num > 0 else { return "1이상의 정수만 지원합니다." }
         
