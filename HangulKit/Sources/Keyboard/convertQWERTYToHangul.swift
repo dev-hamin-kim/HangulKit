ed1345f49fa1029f7830dda95a52e0b0bc4fee43
dev-hamin-kim <129831340+dev-hamin-kim@users.noreply.github.com>
2025-04-29 18:33:59 +0900

    docs: Keyboard/convert 3종세트 메서드 주석 추가

--- HangulKit/Sources/Keyboard/convertQWERTYToHangul.swift
+++ HangulKit/Sources/Keyboard/convertQWERTYToHangul.swift
@@ -7,6 +7,12 @@
 
 public extension Hangul {
     
+    /// QWERTY 자판에서 입력된 영어 알파벳을 동일한 위치의 두벌식 자판에 해당하는 한글 음소로 변환한 후 온전한 한글 문자와 문장으로 변환합니다
+    ///
+    ///     Hangul.convertQWERTYToHangul("abc") // returns "뮻"
+    ///     Hangul.convertQWERTYToHangul("ABC") // also returns "뮻"
+    ///     Hangul.convertQWERTYToHangul("dkdldhdptm") // returns "아이오에스"
+    ///     Hangul.convertQWERTYToHangul("dkdl오에스") // also returns "아이오에스"
     static func convertQWERTYToHangul(_ word: String) -> String {
         if word.isEmpty { return "" }
         
