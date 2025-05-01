e57f754861343c631d60c9839d09bddc9746e8f0
dev-hamin-kim <129831340+dev-hamin-kim@users.noreply.github.com>
2025-05-02 08:42:25 +0900

    test: HangulKitWrapper 변수명 변경에 따른 테스트 코드 수정

--- HangulKitTests/HangulKitTests.swift
+++ HangulKitTests/HangulKitTests.swift
@@ -12,29 +12,29 @@ struct HangulKitTests {
 
     // isHangulSyllable은 완성된 한글 문자를 받으면 true를 반환한다.
     @Test func isHangulSyllable() async throws {
-        #expect("가".hangul.isHangulSyllable() == true)
-        #expect("값".hangul.isHangulSyllable() == true)
-        #expect("ㄱ".hangul.isHangulSyllable() == false)
-        #expect("ㅏ".hangul.isHangulSyllable() == false)
-        #expect("a".hangul.isHangulSyllable() == false)
+        #expect("가".hk.isHangulSyllable() == true)
+        #expect("값".hk.isHangulSyllable() == true)
+        #expect("ㄱ".hk.isHangulSyllable() == false)
+        #expect("ㅏ".hk.isHangulSyllable() == false)
+        #expect("a".hk.isHangulSyllable() == false)
     }
     
     // isHangulAlphabet은 조합되지않은 한글 문자를 받으면 true를 반환한다.
     @Test func isHangulAlphabet() async throws {
-        #expect("가".hangul.isHangulAlphabet() == false)
-        #expect("값".hangul.isHangulAlphabet() == false)
-        #expect("ㄱ".hangul.isHangulAlphabet() == true)
-        #expect("ㅏ".hangul.isHangulAlphabet() == true)
-        #expect("a".hangul.isHangulAlphabet() == false)
+        #expect("가".hk.isHangulAlphabet() == false)
+        #expect("값".hk.isHangulAlphabet() == false)
+        #expect("ㄱ".hk.isHangulAlphabet() == true)
+        #expect("ㅏ".hk.isHangulAlphabet() == true)
+        #expect("a".hk.isHangulAlphabet() == false)
     }
     
     // isHangul은 한글 문자열을 받으면 true를 반환한다.
     @Test func isHangul() async throws {
-        #expect("값".hangul.isHangul() == true)
-        #expect("ㄱ".hangul.isHangul() == true)
-        #expect("ㅏ".hangul.isHangul() == true)
-        #expect("저는 고양이를 좋아합니다".hangul.isHangul() == true)
-        #expect("a a".hangul.isHangul() == false)
+        #expect("값".hk.isHangul() == true)
+        #expect("ㄱ".hk.isHangul() == true)
+        #expect("ㅏ".hk.isHangul() == true)
+        #expect("저는 고양이를 좋아합니다".hk.isHangul() == true)
+        #expect("a a".hk.isHangul() == false)
     }
 
 }
