0bace6f3f071cab9061b8321f91c31d79bfe866d
dev-hamin-kim <129831340+dev-hamin-kim@users.noreply.github.com>
2025-06-10 19:38:47 +0900

    docs: 튜토리얼 내부 "설치하기" 섹션 작성 완료 및 스크린샷/코드 예시 추가

---
+++ Sources/HangulKit/HangulKit.docc/Resources/tutorial-examples/01-ViewController-1.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+final class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
---
+++ Sources/HangulKit/HangulKit.docc/Resources/tutorial-examples/01-ViewController-2.swift
@@ -0,0 +1,10 @@
+import UIKit
+import HangulKit
+
+final class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
